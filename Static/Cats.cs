5a8af08d4e9b9f0f6624ef3464e3d2ed31f1c119
alexandriaware <ali.mercer92@gmail.com>
2017-10-13 09:47:57 -0400

    Added Cats class

---
+++ Static/Cats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Cats
+    {
+        private int catCount;
+
+        public int CatCount
+        {
+            get { return this.catCount; }
+            set { this.catCount = value; }
+        }
+
+        public Cats()
+        {
+            //default
+        }
+
+        public void WarCry()
+        {
+            Console.WriteLine("We are an army of " + catCount + " cats!!");
+        }
+    }
+}
