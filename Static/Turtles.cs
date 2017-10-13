c24da037d45b2757411759608ccd6e01f15eaa61
alexandriaware <ali.mercer92@gmail.com>
2017-10-13 09:52:11 -0400

    Added Turtles class

---
+++ Static/Turtles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Turtles
+    {
+        static int turtleCount; // <-- static doesn't need a property
+
+        public Turtles()
+        {
+            turtleCount++;
+        }
+
+        public static void WarCry()
+        {
+            Console.WriteLine("We are an army of " + turtleCount + " turtles!!");
+        }
+
+
+
+    }
+}
